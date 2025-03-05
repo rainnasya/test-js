409b6dbbe2bf7bad63ec3c4de98e1efc945354c7
Rainnasya <rainnasyaaca@gmail.com>
2025-03-05 11:09:27 +0700

    Update login.js: perbaikan validasi

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const errorMessage = document.getElementById("errorMessage");
 
     loginForm.addEventListener("submit", function (event) {
-        event.preventDefault();
+        event.preventDefault(); 
 
         const username = document.getElementById("username").value.trim();
         const password = document.getElementById("password").value.trim();
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
             errorMessage.textContent = "Username dan Password harus diisi!";
             return;
         }
+
         loginUser(username, password);
     });
 });
@@ -20,16 +21,24 @@ async function loginUser(username, password) {
     const errorMessage = document.getElementById("errorMessage");
 
     try {
-        const response = await fetch("http://localhost:3000/users?username=" + username);
+        
+        const response = await fetch(`http://localhost:3000/users?username=${username}`);
+        const users = await response.json(); 
 
-        const result = await response.json();
-
-        if (response.ok) {
-            alert("Login berhasil!");
-            window.location.href = " "; 
-        } else {
-            errorMessage.textContent = result.message;
+        if (users.length === 0) {
+            errorMessage.textContent = "Username tidak ditemukan!";
+            return;
         }
+
+        const user = users[0];
+
+        if (user.password !== password) {
+            errorMessage.textContent = "Password salah!";
+            return;
+        }
+        alert("Login berhasil!");
+        window.location.href = "dashboard.html";
+
     } catch (error) {
         errorMessage.textContent = "Terjadi kesalahan. Coba lagi nanti!";
     }
